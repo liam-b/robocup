b142e981d32333e78fad4a92e4147f3dec91c89c
Liam Brennan <liam@anywhich.com>
2017-03-05 19:56:08 +1100

    removed test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const readline = require('readline');
-
-readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(true);
-
-process.stdin.on('keypress', (str, key) => {
-  console.log(str)
-  console.log(key)
-})
