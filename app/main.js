20a1522331d7d47db567f0c52e180c77bd7730af
Daniel <danielhp@optusnet.com.au>
2017-05-07 09:40:42 +1000

    Should now be able to set north on setup

--- app/main.js
+++ app/main.js
@@ -80,7 +80,7 @@ buttons.event.pressed('back', function () {
 });
 
 buttons.event.pressed('left', function () {
-  if (constants.PAUSED){
+  if (constants.PAUSED || constants.BOT_STATE != 'looping'){
     bot.compass.setRelativeNorth(bot.compass.value());
     output.log('set', 'relative north set');
   }
