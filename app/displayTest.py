f125c0c9134a8ed0763b0f5e8edf906e37341cf6
Daniel <danielhp@optusnet.com.au>
2017-05-07 13:56:08 +1000

    testing lcd display - new sample

--- app/displayTest.py
+++ app/displayTest.py
@@ -1,41 +1,25 @@
-#!/usr/bin/env python
+from time import sleep
+import ev3dev.auto as ev3
 
-# Hard coding these values is not a good idea because the values could
-# change. But, since this is an example, we want to keep it short.
-SCREEN_WIDTH = 178 # pixels
-SCREEN_HEIGHT = 128 # pixels
-LINE_LENGTH = 24 # bytes
-SIZE = 3072 # bytes
+screen = ev3.Screen()
 
-import os
-import array
+smile = True
 
+while True:
+    screen.clear()
 
-def main():
-    buf = [0] * SIZE
+    # Screen.draw returns a PIL.ImageDraw handle
+    screen.draw.ellipse(( 20, 20,  60, 60))
+    screen.draw.ellipse((118, 20, 158, 60))
 
-    # draw a vertical line in column 100 (0 based index)
-    for row in range(0, SCREEN_HEIGHT):
-        buf[row * LINE_LENGTH + int(89 / 8)] = 1 << (89 % 8)
+    if smile:
+        screen.draw.arc((20, 80, 158, 100), 0, 180)
+    else:
+        screen.draw.arc((20, 80, 158, 100), 180, 360)
 
-    # draw a horizontal line in row 64 (0 based index)
-    for col in range(0, LINE_LENGTH):
-        buf[64 * LINE_LENGTH + col] = 0xff
+    smile = not smile
 
+    # Update lcd display
+    screen.update()
 
-    import math
-    # draw a circle, center at (40,40), radius is 20
-    for x in range(0, 20):
-        y = math.sqrt(20 * 20 - x * x)
-        buf[(40 + int(y)) * LINE_LENGTH + int((40 + x) / 8)] = 1 << ((40 + x) % 8)
-        buf[(40 - int(y)) * LINE_LENGTH + int((40 + x) / 8)] = 1 << ((40 + x) % 8)
-        buf[(40 + int(y)) * LINE_LENGTH + int((40 - x) / 8)] = 1 << ((40 - x) % 8)
-        buf[(40 - int(y)) * LINE_LENGTH + int((40 - x) / 8)] = 1 << ((40 - x) % 8)
-
-    f = os.open('/dev/fb0', os.O_RDWR)
-    s = array.array('B', buf).tostring()
-    os.write(f, s)
-    os.close(f)
-
-if __name__ == '__main__':
-    main()
+    sleep(1)
