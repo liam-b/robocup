791b101f8a38d3e6ba5f27fde95a94b03109ce7e
Liam Brennan <liam@anywhich.com>
2017-05-18 15:50:17 +1000

    testing kicker

--- app/behavior/kick.js
+++ app/behavior/kick.js
@@ -2,12 +2,14 @@
 // also specs will be needed
 
 module.exports.reset = function (motor) {
-  console.log('reset start');
-  motor.run(-40); // probbably find a speed better than this
-  setTimeout(function () {
-    motor.stop();
-    console.log('reset end');
-  }, 1000); // find a delay that won't over rotate the motor
+  module.exports.kick(motor)
+  
+  // console.log('reset start');
+  // motor.run(-40); // probbably find a speed better than this
+  // setTimeout(function () {
+  //   motor.stop();
+  //   console.log('reset end');
+  // }, 1000); // find a delay that won't over rotate the motor
 }
 
 module.exports.kick = function (motor) {
