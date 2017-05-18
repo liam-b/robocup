8c703efe840bc8fb013899532c583997c0d0fa1b
Liam Brennan <liam@anywhich.com>
2017-05-18 15:25:00 +1000

    kick editing and more

--- app/behavior/kick.js
+++ app/behavior/kick.js
@@ -2,18 +2,22 @@
 // also specs will be needed
 
 module.exports.reset = function (motor) {
+  console.log('reset start');
   motor.run(100); // probbably find a speed better than this
   setTimeout(function () {
     motor.stop();
+    console.log('reset end');
   }, 1000); // find a delay that won't over rotate the motor
 }
 
 module.exports.kick = function (motor) {
+  console.log('kick start');
   motor.run(-800);
   setTimeout(function () {
     motor.run(800);
     setTimeout(function () {
       motor.stop();
+      console.log('kick end');
     }, 200); // again find appropriate delays
   }, 200);
 }
---
+++ app/kickTest.js
@@ -0,0 +1,18 @@
+var Logger = require('./log.js');
+var constants = require('./constants.js');
+
+var motor = require('./io/motor.js');
+var sensor = require('./io/sensor.js');
+var extra = require('./io/extra.js');
+
+var kick = require('./behavior/kick.js')
+
+var leds = new extra.Leds();
+var output = new Logger(leds, (process.argv[2] == 'quiet'));
+
+var motor = new motor.Motor('outD', output);
+
+kick.reset(motor);
+setTimeout(function () {
+  kick.kick(motor);
+}, 3000);
