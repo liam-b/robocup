03d84954a25b3421ee8cb6642cd4ce94eca74477
Liam Brennan <liam@anywhich.com>
2017-05-18 15:32:02 +1000

    testing kicker

--- app/behavior/kick.js
+++ app/behavior/kick.js
@@ -14,10 +14,10 @@ module.exports.kick = function (motor) {
   console.log('kick start');
   motor.run(800);
   setTimeout(function () {
-    motor.run(-800);
+    motor.run(-100);
     setTimeout(function () {
       motor.stop();
       console.log('kick end');
-    }, 200); // again find appropriate delays
+    }, 1000); // again find appropriate delays
   }, 200);
 }
