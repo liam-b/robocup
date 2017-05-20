296b819851d788cf61743405a772e3c7d4c69969
Daniel <danielhp@optusnet.com.au>
2017-05-20 16:10:20 +1000

    testing function

--- app/helper/network/network.sh
+++ app/helper/network/network.sh
@@ -3,10 +3,10 @@
 write_text () {
     echo $1 # > in.txt
 }
-
-if [ $1 = "NRC1" ]; then
-  ssh robot@192.168.43.242
-  cd ~/robocup/app/helper/network
-  write_text $2
-  exit
-fi
+write_text $2
+# if [ $1 = "NRC1" ]; then
+#   ssh robot@192.168.43.242
+#   cd ~/robocup/app/helper/network
+#   write_text $2
+#   exit
+# fi
