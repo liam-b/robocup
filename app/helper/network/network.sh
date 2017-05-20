7c685134a49306d1266c27fde54e5826d8d8fc19
Daniel <danielhp@optusnet.com.au>
2017-05-20 16:00:32 +1000

    changed function name

--- app/helper/network/network.sh
+++ app/helper/network/network.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-function write {
+function write_text {
     echo $1 # > in.txt
 }
 
 if [$1 == 'NRC1']; then
   ssh robot@192.168.43.242
   cd ~/robocup/app/helper/network
-  write $2
+  write_text $2
   exit
 fi
