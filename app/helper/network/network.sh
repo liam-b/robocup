afa6b8ec7b39b825fbe5d918b62a159d4b02f155
Daniel <danielhp@optusnet.com.au>
2017-05-20 15:31:26 +1000

    messing with parameters

--- app/helper/network/network.sh
+++ app/helper/network/network.sh
@@ -1,10 +1,10 @@
-function write(text) {
-  echo text > in.txt
+function write {
+  echo $1 > in.txt
 }
 
 if [$1 == 'NRC1']; then
   ssh robot@192.168.43.242
   cd ~/robocup/app/helper/network
-  write($2)
+  write $2
   exit
 fi
