d515647301652898ebb5af3076d716efeb78cef0
Daniel <danielhp@optusnet.com.au>
2017-05-25 16:26:33 +1000

    eh

--- app/helper/network/network.sh
+++ app/helper/network/network.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
+# First argument is the bot's name, second is the command/message to be sent
 
 write_text () {
     echo $1 > in.txt
 }
 write_text $1
 if [ $1 = "NRC1" ]; then
-#   ssh robot@192.168.43.242 # Connect to NRC2
-#   cd ~/robocup/app/helper/network
-  write_text $2
-#   exit
+  ssh robot@192.168.43.242 'cd ~/robocup/app/helper/network; echo $2 > in.txt; exit'
 fi
