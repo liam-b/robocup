92bbca2ae344bee3c30e067c543a6f32ee02a38c
Daniel <danielhp@optusnet.com.au>
2017-05-25 15:25:40 +1000

    test on bot

--- app/helper/network/network.sh
+++ app/helper/network/network.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 write_text () {
-    echo $1 # > in.txt
+    echo $1 > in.txt
 }
-write_text $2
-# if [ $1 = "NRC1" ]; then
-#   ssh robot@192.168.43.242
+write_text $1
+if [ $1 = "NRC1" ]; then
+#   ssh robot@192.168.43.242 # Connect to NRC2
 #   cd ~/robocup/app/helper/network
-#   write_text $2
+  write_text $2
 #   exit
-# fi
+fi
