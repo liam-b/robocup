401934157bb8f50da964402c8ecc455e9126a1e4
Daniel Hewitt-Perraud <danielhp@optusnet.com.au>
2017-06-10 15:41:37 +1000

    Removed cd

--- app/helper/network/network.sh
+++ app/helper/network/network.sh
@@ -7,6 +7,6 @@ write_text () {
 }
 write_text ${args[1]}
 if [ ${args[0]} = "NRC1" ]; then
-  ssh robot@192.168.43.242 "cd ~/robocup/app/helper/network; echo ${args[1]} > in.txt; exit"
+  ssh robot@192.168.43.242 "echo ${args[1]} > ~/robocup/app/helper/network/in.txt; exit"
   echo 'done'
 fi
