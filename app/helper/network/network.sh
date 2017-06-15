32f10255174310a92653778ad7eaf8ca312499f2
Daniel <danielhp@optusnet.com.au>
2017-06-15 16:09:11 +1000

    works between laptops

--- app/helper/network/network.sh
+++ app/helper/network/network.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 # First argument is the bot's name, second is the command/message to be sent
-args=($1 $2)
-write_text () {
-    echo $1 > in.txt
-}
-write_text ${args[1]}
-if [ ${args[0]} = "NRC1" ]; then
-  ssh liam@192.168.43.218 "touch in.txt; echo ${args[1]} > in.txt; exit"
+# write_text () {
+#     echo $1 > in.txt
+# }
+# write_text $2
+if [ $1 = "NRC1" ]; then
+  ssh liam@192.168.43.218 "touch in.txt; echo $2 > in.txt; exit"
   # ssh robot@192.168.43.242 "echo ${args[1]} > ~/robocup/app/helper/network/in.txt; exit"
   echo 'done'
 fi
