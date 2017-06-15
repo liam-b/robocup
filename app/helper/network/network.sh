59fa14008e274a055e7deae52098b6326634e48d
Daniel <danielhp@optusnet.com.au>
2017-06-15 15:29:02 +1000

    test

--- app/helper/network/network.sh
+++ app/helper/network/network.sh
@@ -7,6 +7,6 @@ write_text () {
 }
 write_text ${args[1]}
 if [ ${args[0]} = "NRC1" ]; then
-  ssh robot@192.168.43.242 "cd ~/robocup/app/helper/network; echo ${args[1]} > in.txt; exit"
+  ssh liam@192.168.43.218 "touch in.txt; echo ${args[1]} > in.txt; exit"
   echo 'done'
 fi
