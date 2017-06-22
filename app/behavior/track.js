c4c84ed96133a22a5fafec8d067473a3558f0fdd
liam-b <liam@anywhich.com>
2017-06-22 16:30:19 +1000

    added initial tracking behavior

--- app/behavior/track.js
+++ app/behavior/track.js
@@ -1,39 +1,37 @@
 module.exports = function (motors, speed) {
-  else {
-    switch (angle) {
-      case 9:
-        motors.ratio([0.5, -0.5], speed);
-        break;
-      case 8:
-        motors.ratio([0.4, -0.4], speed);
-        break;
-      case 7:
-        motors.ratio([0.2, -0.2], speed);
-        break;
-      case 6:
-        motors.ratio([0.1, -0.1], speed);
-        break;
-      case 5:
-        motors.ratio([0, 0], speed);
-        break;
-      case 4:
-        motors.ratio([-0.1, 0.1], speed);
-        break;
-      case 3:
-        motors.ratio([-0.2, 0.2], speed);
-        break;
-      case 2:
-        motors.ratio([-0.4, 0.4], speed);
-        break;
-      case 1:
-        motors.ratio([-0.5, 0.5], speed);
-        break;
-      case 0:
-        motors.ratio([0.5, 1.5], speed);
-        break;
-      case undefined:
-        motors.ratio([-1, 1], speed);
-        break;
-    }
+  switch (angle) {
+    case 9:
+      motors.ratio([0.5, -0.5], speed);
+      break;
+    case 8:
+      motors.ratio([0.4, -0.4], speed);
+      break;
+    case 7:
+      motors.ratio([0.2, -0.2], speed);
+      break;
+    case 6:
+      motors.ratio([0.1, -0.1], speed);
+      break;
+    case 5:
+      motors.ratio([0, 0], speed);
+      break;
+    case 4:
+      motors.ratio([-0.1, 0.1], speed);
+      break;
+    case 3:
+      motors.ratio([-0.2, 0.2], speed);
+      break;
+    case 2:
+      motors.ratio([-0.4, 0.4], speed);
+      break;
+    case 1:
+      motors.ratio([-0.5, 0.5], speed);
+      break;
+    case 0:
+      motors.ratio([0.5, 1.5], speed);
+      break;
+    case undefined:
+      motors.ratio([-1, 1], speed);
+      break;
   }
 }
