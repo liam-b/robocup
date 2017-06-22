78f1b1cb115db0bc3d44761e5f073f4ce3e53879
Daniel <danielhp@optusnet.com.au>
2017-06-22 15:57:03 +1000

    equal is still bs

--- app/helper/network/network.sh
+++ app/helper/network/network.sh
@@ -6,7 +6,8 @@
 # }
 # write_text $2
 in_dir="~/robocup/helper/network"
-if [ $1 -eq NRC1 ]; then
+if [ $1 == 'NRC1' ]
+then
   ssh robot@192.168.43.242 "touch ${in_dir}/in.txt; echo $2 > ${in_dir}/in.txt; exit" #TEST!!!
   echo 'done'
 fi
