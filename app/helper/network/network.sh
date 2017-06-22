dfb8377a361acc5f01d01d8055e6315be1af4af2
Daniel <danielhp@optusnet.com.au>
2017-06-22 16:03:22 +1000

    fixed dir

--- app/helper/network/network.sh
+++ app/helper/network/network.sh
@@ -5,7 +5,7 @@
 #     echo $1 > in.txt
 # }
 # write_text $2
-in_dir="~/robocup/helper/network"
+in_dir="~/robocup/app/helper/network"
 if [ $1 == 'NRC1' ]
 then
   ssh robot@192.168.43.242 "touch ${in_dir}/in.txt; echo $2 > ${in_dir}/in.txt; exit" #TEST!!!
