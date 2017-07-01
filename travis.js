bd43c219de37806984ec76f749d6d02f10bc7909
liam-b <liam@anywhich.com>
2017-07-01 20:21:01 +1000

    added trello support

--- travis.js
+++ travis.js
@@ -3,19 +3,47 @@ var request = require('request');
 // http://localhost:3000/
 // https://liam-server-3000.herokuapp.com/
 
+var values = {
+  status: process.argv[2],
+  build: process.argv[3],
+  commit: process.argv[4].substr(0, 7)
+};
+
 var todoist = {
   failed: {
-    content: 'Robocup build ' + process.argv[2] + '[' + process.argv[3] + ']' + ' failed',
-    priority: 4,
+    content: 'Robocup build ' + values.build + ' [' + values.commit + '] failed',
+    priority: 3,
     labels: [2148074807]
   }
 };
 
-request.post({
-  url: 'https://liam-server-3000.herokuapp.com/todoist',
-  form: {
-    data: JSON.stringify(todoist.failed)
+var trello = {
+  failed: {
+    id: values.build,
+    commit: values.commit,
+    status: 'failing'
+  },
+  passed: {
+    id: values.build,
+    commit: values.commit,
+    status: 'passing'
   }
-}, function (error, response, body) {
-  console.log(body);
-});
+};
+
+var services = [
+  {service: 'trello', data: (values.status == 'success') ? trello.passed : trello.failed},
+  {service: 'todoist', data: (values.status == 'success') ? null : todoist.failed}
+];
+
+for (var i = 0; i < services.length; i += 1) {
+  if (services[i].data != null) {
+    request.post({
+      url: 'https://liam-server-3000.herokuapp.com/' + services[i].service,
+      form: {
+        data: JSON.stringify(services[i].data)
+      }
+    }, function (error, response, body) {
+      console.log(body);
+    });
+  }
+}
