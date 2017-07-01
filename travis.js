b64bdd0cdd6ee904eb02e92af01cfa6fad32c061
liam-b <liam@anywhich.com>
2017-07-01 13:53:25 +1000

    fiddled with travis config

--- travis.js
+++ travis.js
@@ -1,11 +1,37 @@
 var request = require('request');
 
-// http://localhost:3000/todoist/add
+// http://localhost:3000/
+// https://liam-server-3000.herokuapp.com/
 
-request.post('https://fathomless-mesa-60224.herokuapp.com/todoist/add', {form: {
-  text: 'Robocup build failed',
-  priority: 4,
-  label: 2148074807
-}}, function (error, response, body) {
+var todoist = {
+  failed: {
+    content: 'Robocup build ' + process.argv[2] + ' failed',
+    priority: 4,
+    labels: [2148074807]
+  }
+};
+
+var notification = {
+  failed: {
+    title: 'Travis CI build failed',
+    message: 'Robocup build failed'
+  }
+};
+
+// request.post({
+//   url: 'https://liam-server-3000.herokuapp.com/notification',
+//   form: {
+//     data: JSON.stringify(notification.failed)
+//   }
+// }, function (error, response, body) {
+//   console.log(body);
+// });
+
+request.post({
+  url: 'https://liam-server-3000.herokuapp.com/todoist',
+  form: {
+    data: JSON.stringify(todoist.failed)
+  }
+}, function (error, response, body) {
   console.log(body);
 });
