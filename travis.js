59cb362bb773d621863315efbd4219876910c6ce
liam-b <liam@anywhich.com>
2017-07-01 10:49:50 +1000

    added more item options

--- travis.js
+++ travis.js
@@ -1,8 +1,11 @@
 var request = require('request');
 
+// http://localhost:3000/todoist/add
+
 request.post('https://fathomless-mesa-60224.herokuapp.com/todoist/add', {form: {
   text: 'Robocup build failed',
-  priority: 1
+  priority: 4,
+  label: 2148074807
 }}, function (error, response, body) {
   console.log(body);
 });
