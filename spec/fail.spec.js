5d73ffa236dcf53b9f2de3f753b4328bec909217
liam-b <liam@anywhich.com>
2017-07-01 20:35:38 +1000

    removed dummy fail spec

--- spec/fail.spec.js
+++
@@ -1,7 +0,0 @@
-var detail = require('./services/color.js');
-
-detail('dummy fail thing', 'fail', __filename, function () {
-  it('will fail', function () {
-    expect(true).toBe(false);
-  });
-});
