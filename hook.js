a021394eee08a1434bbeb4070fc3a3df737fc8b8
liam-b <liam@anywhich.com>
2017-07-02 13:39:33 +1000

    23: hmm

--- hook.js
+++ hook.js
@@ -1,15 +1,22 @@
 var request = require('request');
 
+console.log('2', process.argv[2]);
+console.log('3', process.argv[3]);
+
 var values = {
   message: process.argv[2],
   id: process.argv[3]
 };
 
+// console.log(process.argv[2]);
+
 var commit = {
   message: values.message,
-  link: 'https://github.com/liam-b/robocup/commit/' + values.message,
-  id: values.id.substr(0, 2)
+  link: 'https://github.com/liam-b/robocup/commit/' + values.id,
+  id: values.message.substr(0, 2)
 };
+
+console.log('c', commit);
 
 request.post({
   url: 'https://liam-server-3000.herokuapp.com/commit-trello',
