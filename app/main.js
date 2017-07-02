85309d9e89ccfe99c249a4830e378e635b5765e8
liam-b <liam@anywhich.com>
2017-07-02 14:21:53 +1000

    another test commit

--- app/main.js
+++ app/main.js
@@ -1,8 +1,6 @@
 var Logger = require('./log.js');
 var constants = require('./constants.js');
 
-//main
-
 var motor = require('./io/motor.js');
 var sensor = require('./io/sensor.js');
 var extra = require('./io/extra.js');
