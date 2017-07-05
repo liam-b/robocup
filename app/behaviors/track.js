7b352cbbc1cb24b4eb5134e2b97fb7d9077545b1
liam-b <liam@anywhich.com>
2017-07-05 11:59:56 +1000

    #102 made tracking faster relative to speed const

--- app/behaviors/track.js
+++ app/behaviors/track.js
@@ -5,34 +5,34 @@ module.exports = function (motors, seeker, speed) {
 
   switch (values.angle) {
     case 9:
-      motors.ratio([0.5, -0.5], speed);
+      motors.ratio([1, -1], speed);
       break;
     case 8:
-      motors.ratio([0.4, -0.4], speed);
+      motors.ratio([0.8, -0.8], speed);
       break;
     case 7:
-      motors.ratio([0.2, -0.2], speed);
+      motors.ratio([0.6, -0.6], speed);
       break;
     case 6:
-      motors.ratio([0.1, -0.1], speed);
+      motors.ratio([0.4, -0.4], speed);
       break;
     case 5:
       motors.ratio([0, 0], speed);
       break;
     case 4:
-      motors.ratio([-0.1, 0.1], speed);
-      break;
-    case 3:
-      motors.ratio([-0.2, 0.2], speed);
-      break;
-    case 2:
       motors.ratio([-0.4, 0.4], speed);
       break;
+    case 3:
+      motors.ratio([-0.6, 0.6], speed);
+      break;
+    case 2:
+      motors.ratio([-0.8, 0.8], speed);
+      break;
     case 1:
-      motors.ratio([-0.5, 0.5], speed);
+      motors.ratio([-1, 1], speed);
       break;
-    case 0:
-      motors.ratio([1, -1], speed);
-      break;
+    // case 0:
+    //   motors.ratio([1, -1], speed);
+    //   break;
   }
 };
