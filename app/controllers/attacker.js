5201630f3b4828eef64f4c0876c68ed2ae8b9009
Daniel <danielhp@optusnet.com.au>
2017-07-05 12:29:37 +1000

    #10 removefd default

--- app/controllers/attacker.js
+++ app/controllers/attacker.js
@@ -1,4 +1,4 @@
-module.exports.default = function (bot, behaviors, helpers, constants) { STATE[constants.ATTACKER.STATE](bot, behaviors, helpers, constants); };
+module.exports = function (bot, behaviors, helpers, constants) { STATE[constants.ATTACKER.STATE](bot, behaviors, helpers, constants); };
 
 var STATE = {
   'dribble': function (bot, behaviors, helpers, constants) {
