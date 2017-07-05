568482c386bcba658fac5629ff674b89f3595b54
liam-b <liam@anywhich.com>
2017-07-05 10:40:18 +1000

    added state log

--- app/test.js
+++ app/test.js
@@ -113,6 +113,7 @@ function loop () {
   // var seekerValues = bot.seeker.value();
   // console.log(seekerValues);
   // behaviors.chase(bot.motors, seekerValues.angle, seekerValues.distance, constants.CHASE_SPEED);
+  output.trace('state', constants.DEFENDER.STATE);
   controllers.defender(bot, behaviors, helpers, constants);
 }
 
