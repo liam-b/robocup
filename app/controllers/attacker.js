b92b022b53dbe4198633e3882c6e2cce0d44d1aa
Daniel <danielhp@optusnet.com.au>
2017-07-12 12:03:05 +1000

    removed debugging stuff

--- app/controllers/attacker.js
+++ app/controllers/attacker.js
@@ -2,18 +2,13 @@ module.exports = function (bot, behaviors, helpers, constants) { STATE[constants
 
 var STATE = {
   'dribble': function (bot, behaviors, helpers, constants) {
-    console.log('dribbing');
     behaviors.chase(bot.motors, constants, bot.seeker);
-    console.log('ahhh...')
     if (bot.colorSensor.value() < constants.FIELD.BLACK_PCT) {
-      console.log('what')
       constants.ATTACKER.STATE = 'shoot';
     }
-    console.log('hmmm')
   },
   'shoot': function (bot, behaviors, helpers, constants) {
     if (!constants.ATTACKER.SHOOTING) {
-      console.log('shooting');
       constants.ATTACKER.SHOOTING = true;
       bot.motors.ratio([1, 1], constants.ATTACKER.SHOOT_SPEED);
       setTimeout(behaviors.kick(bot.kicker), 500);
