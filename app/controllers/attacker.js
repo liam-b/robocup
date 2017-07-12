6d191f3295f41ea7bcb99e80c1e05ae026b943ab
Daniel <danielhp@optusnet.com.au>
2017-07-12 10:12:55 +1000

    Testing shooting

--- app/controllers/attacker.js
+++ app/controllers/attacker.js
@@ -4,7 +4,7 @@ var STATE = {
   'dribble': function (bot, behaviors, helpers, constants) {
     behaviors.chase(bot.motors, constants, bot.seeker);
     if (bot.ColorSensor.value() < 40) {
-      constants.attacker.state = 'shoot';
+      constants.attacker.STATE = 'shoot';
     }
   },
   'shoot': function (bot, behaviors, helpers, constants) {
@@ -12,7 +12,7 @@ var STATE = {
       constants.SHOOTING = true;
       bot.motors.ratio([1, 1], constants.attacker.SHOOT_SPEED);
       setTimeout(behaviors.kick(bot.kicker), 500);
-      setTimeout(function (){bot.motors.stop; constants.SHOOTING = false}, 1000);
+      setTimeout(function (){bot.motors.stop; constants.SHOOTING = false; constants.attacker.STATE = 'dribble'}, 1000);
     }
   }
 };
