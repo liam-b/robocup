04e51ee614b2ef7b72debce88ede8894b8283051
Daniel <danielhp@optusnet.com.au>
2017-07-12 12:31:52 +1000

    will now only shoot if facing (roughly) the goals

--- app/controllers/attacker.js
+++ app/controllers/attacker.js
@@ -3,12 +3,12 @@ module.exports = function (bot, behaviors, helpers, constants) { STATE[constants
 var STATE = {
   'dribble': function (bot, behaviors, helpers, constants) {
     behaviors.chase(bot.motors, constants, bot.seeker);
-    if (bot.colorSensor.value() <= constants.FIELD.BLACK_PCT) {
+    if (bot.colorSensor.value() <= constants.FIELD.BLACK_PCT && bot.compass.value() > 315 && bot.compass.value() < 45) {
       constants.ATTACKER.STATE = 'shoot';
     }
   },
   'shoot': function (bot, behaviors, helpers, constants) {
-    if (!constants.ATTACKER.SHOOTING && bot.compass.value() > 315 && bot.compass.value() < 45) {
+    if (!constants.ATTACKER.SHOOTING) {
       constants.ATTACKER.SHOOTING = true;
       bot.motors.ratio([1, 1], constants.ATTACKER.SHOOT_SPEED);
       setTimeout(behaviors.kick(bot.kicker), 250);
