1bde01a0c336dde594610251e7fba436b92754ae
liam-b <liam@anywhich.com>
2017-07-12 14:04:34 +1000

    moved around params again

--- app/controllers/defender.js
+++ app/controllers/defender.js
@@ -11,7 +11,7 @@ var STATE = {
       constants.DEFENDER.STATE = 'intercept';
     }
     else {
-      behaviors.track(bot.motors, constants.TRACK_SPEED, bot.seeker);
+      behaviors.track(bot.motors, bot.seeker, constants.TRACK_SPEED);
     }
   },
   'intercept': function (bot, behaviors, helpers, constants) {
@@ -22,7 +22,7 @@ var STATE = {
       constants.DEFENDER.STATE = 'return';
     }
     else {
-      behaviors.chase(bot.motors, bot.seeker, constants.CHASE_SPEED);
+      behaviors.chase(bot.motors, constants.CHASE_SPEED, bot.seeker);
     }
   },
   'return': function (bot, behaviors, helpers, constants) {
