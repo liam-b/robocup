9e3bdaf5f2e3f6d6cd20f6dcb33d4426a8e1a812
liam-b <liam@anywhich.com>
2017-07-20 15:43:28 +1000

    made traking overshoot less

--- app/behaviors/track.js
+++ app/behaviors/track.js
@@ -5,13 +5,13 @@ module.exports = function (motors, seeker, speed) {
 
   switch (values.angle) {
     case 9:
-      motors.ratio([1, -1], speed);
+      motors.ratio([0.9, -0.9], speed);
       break;
     case 8:
-      motors.ratio([0.8, -0.8], speed);
+      motors.ratio([0.6, -0.6], speed);
       break;
     case 7:
-      motors.ratio([0.6, -0.6], speed);
+      motors.ratio([0.4, -0.4], speed);
       break;
     case 6:
       motors.ratio([0.2, -0.2], speed);
@@ -23,13 +23,13 @@ module.exports = function (motors, seeker, speed) {
       motors.ratio([-0.2, 0.2], speed);
       break;
     case 3:
-      motors.ratio([-0.6, 0.6], speed);
+      motors.ratio([-0.4, 0.4], speed);
       break;
     case 2:
-      motors.ratio([-0.8, 0.8], speed);
+      motors.ratio([-0.6, 0.6], speed);
       break;
     case 1:
-      motors.ratio([-1, 1], speed);
+      motors.ratio([-0.9, 0.9], speed);
       break;
     case 0:
       motors.ratio([-0.3, 0.3], speed);
