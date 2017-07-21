e479dd378456b49f22934a615b62f515b67bd4b6
liam-b <liam@anywhich.com>
2017-07-22 09:23:32 +1000

    trying to recover chase

--- app/behaviors/chase.js
+++ app/behaviors/chase.js
@@ -1,45 +1,39 @@
-var _timeSinceLostBall = 0;
-
 module.exports = function (motors, constants, seeker) {
   var values = seeker.value();
-
-  // values.angle);
-
-  if (true) {
-    switch (values.angle) {
-      case 9:
-        motors.ratio([0.5, -0.5], constants.CHASE_SPEED);
-        break;
-      case 8:
-        motors.ratio([1.7, 0], constants.CHASE_SPEED);
-        break;
-      case 7:
-        motors.ratio([1.5, 0.5], constants.CHASE_SPEED);
-        break;
-      case 6:
-        motors.ratio([1.3, 0.7], constants.CHASE_SPEED);
-        break;
-      case 5:
-        // console.log('eh?'); // s
-        motors.ratio([1, 1], constants.CHASE_SPEED);
-        break;
-      case 4:
-        motors.ratio([0.7, 1.3], constants.CHASE_SPEED);
-        break;
-      case 3:
-        motors.ratio([0.5, 1.5], constants.CHASE_SPEED);
-        break;
-      case 2:
-        motors.ratio([0, 1.7], constants.CHASE_SPEED);
-        break;
-      case 1:
-        motors.ratio([-0.5, 0.5], constants.CHASE_SPEED);
-        break;
-      case 0:
-        motors.ratio([0.5, 1.5], constants.CHASE_SPEED);
-        break;
-    }
-  } else {
-    motors.ratio([1, 1], constants.CHASE_SPEED);
+  switch (values.angle) {
+    case 9:
+      motors.ratio([0.5, -0.5], constants.CHASE_SPEED);
+      break;
+    case 8:
+      motors.ratio([1.7, 0], constants.CHASE_SPEED);
+      break;
+    case 7:
+      motors.ratio([1.5, 0.5], constants.CHASE_SPEED);
+      break;
+    case 6:
+      motors.ratio([1.3, 0.7], constants.CHASE_SPEED);
+      break;
+    case 5:
+      // console.log('eh?'); // s
+      motors.ratio([1, 1], constants.CHASE_SPEED);
+      break;
+    case 4:
+      motors.ratio([0.7, 1.3], constants.CHASE_SPEED);
+      break;
+    case 3:
+      motors.ratio([0.5, 1.5], constants.CHASE_SPEED);
+      break;
+    case 2:
+      motors.ratio([0, 1.7], constants.CHASE_SPEED);
+      break;
+    case 1:
+      motors.ratio([-0.5, 0.5], constants.CHASE_SPEED);
+      break;
+    case 0:
+      motors.ratio([0.5, 1.5], constants.CHASE_SPEED);
+      break;
   }
+  // } else {
+  //   motors.ratio([1, 1], constants.CHASE_SPEED);
+  // }
 };
