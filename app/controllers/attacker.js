520c00b948a2e7c23bf4f80a0963af52f7e3e59a
Daniel <danielhp@optusnet.com.au>
2017-07-22 10:36:10 +1000

    simplified shoot

--- app/controllers/attacker.js
+++ app/controllers/attacker.js
@@ -4,21 +4,21 @@ var STATE = {
   'dribble': function (bot, behaviors, helpers, constants) {
     // bot.motors.ratio([1,1],250);
     behaviors.chase(bot.motors, constants, bot.seeker);
-    if (bot.colorSensor.value() <= constants.FIELD.BLACK_PCT && (helpers.position.relativeRotation() > 315 || helpers.position.relativeRotation() < 45)) {
+    if (bot.seeker.value().distance < constants.KICK_RANGE && (helpers.position.relativeRotation() > 315 || helpers.position.relativeRotation() < 45)) {
       constants.ATTACKER.STATE = 'shoot';
     }
   },
   'shoot': function (bot, behaviors, helpers, constants) {
-    if (!constants.ATTACKER.SHOOTING) {
-      constants.ATTACKER.SHOOTING = true;
-      output.info('attack', 'shooting')
-      bot.motors.ratio([1, 1], constants.ATTACKER.SHOOT_SPEED);
-      setTimeout(behaviors.kick(bot.kicker), 200);
-      setTimeout(function (){
-        bot.motors.stop();
-        constants.ATTACKER.SHOOTING = false;
-        constants.ATTACKER.STATE = 'dribble';
-      }, 500);
+    // if (!constants.ATTACKER.SHOOTING) {
+    //   constants.ATTACKER.SHOOTING = true;
+    output.info('attack', 'shooting');
+    bot.motors.ratio([1, 1], constants.ATTACKER.SHOOT_SPEED);
+    behaviors.kick(bot.kicker)
+      // setTimeout(function (){
+        // bot.motors.stop();
+        // constants.ATTACKER.SHOOTING = false;
+    constants.ATTACKER.STATE = 'dribble';
+      // }, 500);
     }
   }
 };
