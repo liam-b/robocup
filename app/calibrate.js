d351910ec78e8e27244cff4d0ac7c3eb3a6278b1
Daniel <danielhp@optusnet.com.au>
2017-07-28 15:57:07 +1000

    Added direction to calibrate

--- app/calibrate.js
+++ app/calibrate.js
@@ -4,6 +4,10 @@ var sensor = require('./io/sensor.js');
 var buttons = require('./io/buttons.js');
 var extra = require('./io/extra.js');
 
+var helpers = {
+  'position': require('./helpers/position.js')
+};
+
 var leds = new extra.Leds();
 var output = new Logger(leds, (process.argv[2] == 'quiet'));
 
@@ -12,18 +16,21 @@ output.debug('start', 'started');
 var colorSensor = new sensor.ColorSensor('in1', output);
 var ultrasonicSensor = new sensor.UltrasonicSensor('in2', output);
 var seeker = new sensor.SeekerSensor('in3:i2c8', output);
+var compass = new sensor.CompassSensor('in4:i2c1', output);
 
 output.info('start', 'checking connections');
 
 colorSensor.check();
 ultrasonicSensor.check();
 seeker.check();
+compass.check()
 
 output.info('start', 'setting modes');
 
 colorSensor.mode(colorSensor.REFLECTIVE);
 ultrasonicSensor.mode(ultrasonicSensor.DISTANCE);
 seeker.mode(seeker.MODULATED);
+helpers.position.setRelativeNorth(bot.compass.value())
 
 buttons.event.pressed('back', function () {
   output.debug('end', 'ending program');
@@ -53,5 +60,7 @@ function loop() {
     case 'seeker':
       output.debug('calibrate', 'seeker value: ' + seeker.value());
       break;
+    case 'direction':
+        output.debug('calibrate', 'direction: ' + helpers.position.relativeRotation(compass.value()))
   }
 }
