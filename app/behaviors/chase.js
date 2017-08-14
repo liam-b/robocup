4d3a1469c4c4f3a8ca4dac79687c59f610c8e864
Daniel <danielhp@optusnet.com.au>
2017-08-15 09:55:19 +1000

    Now reverse on lost ball

--- app/behaviors/chase.js
+++ app/behaviors/chase.js
@@ -31,7 +31,7 @@ module.exports = function () {
       bot.motors.ratio([-0.5, 0.5], constants.CHASE_SPEED);
       break;
     case 0:
-      bot.motors.ratio([0.25, 1.75], constants.CHASE_SPEED);
+      bot.motors.ratio([-0.25, -1.75], constants.CHASE_SPEED);
       break;
   }
   // } else {
